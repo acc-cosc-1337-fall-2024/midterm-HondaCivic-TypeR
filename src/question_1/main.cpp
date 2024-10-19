0b73a76b0ee8d37359dcaae89278a99cae918213
HondaCivic-TypeR <francisco.rodriguez22@g.austincc.edu>
2024-10-19 04:06:48 +0000

    QUESTION_1

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,33 @@
+#include "question1.h"
+#include<iostream>
 
+using std::cout; using std::cin;
 int main()
 {
+    auto entry = 0;
+    auto widget = 0;
+
+    cout<<"Hello.\n";
+    cout<<"To use our point calculator, press '1'.\n";
+    cout<<"If you don't want to use our calculator, press any other number.\n";
+    cin>>entry;
+
+    while (entry == 1)
+    {
+        cout<<"Please enter your total number of widgets sold: \n";
+        cin>>widget;
+        cout<<"Your total earned points is: "<<get_earned_points(widget)<<"\n\n\n";
+
+        cout<<"Would you like to use the calculator again? If so, press '1' again.\n";
+        cout<<"If not, press any other number.\n";
+        cin>>entry;
+    }
+
+    if(entry != 1)
+    {
+        cout<<"Looks like you don't want to use the calcuator.\n";
+        cout<<"Goodbye.\n";
+    }
 
     return 0;
 }
--- src/question_1/question1.cpp
+++ src/question_1/question1.cpp
@@ -4,3 +4,27 @@ bool test_config()
 {
     return true;
 }
+
+int get_earned_points(int sold)
+{
+    int pointsEarned = 1;
+
+    if(sold >= 1 && sold <= 5)
+    {
+        pointsEarned = sold * 1;
+    }
+    if(sold >= 6 && sold <= 10)
+    {
+        pointsEarned = sold * 5;
+    }
+    if(sold >= 11 && sold <= 15)
+    {
+        pointsEarned = sold * 10;
+    }
+    if(sold >= 16)
+    {
+        pointsEarned = sold * 15;
+    }
+
+    return pointsEarned;
+}
