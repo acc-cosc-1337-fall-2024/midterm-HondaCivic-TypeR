b39d8a2987bf51d18cb31729102067f66181cb41
HondaCivic-TypeR <francisco.rodriguez22@g.austincc.edu>
2024-10-20 04:19:53 +0000

    QUESTION_4.2

--- src/question_4/main.cpp
+++ src/question_4/main.cpp
@@ -5,10 +5,10 @@ using std::cout; using std::cin;
 
 int main()
 {
-    //using 1729397652 as of 11:14PM
-    int hours = get_hours(1729397652);
-    int minutes = get_minutes(1729397652);
-    int seconds = get_seconds(1729397652);
+    //using 1570875018, provided from second test case
+    int hours = get_hours(1570875018);
+    int minutes = get_minutes(1570875018);
+    int seconds = get_seconds(1570875018);
 
     cout<<"It has been "<<hours<<" hours, "<<minutes<<" minutes, and "<<seconds<<" seconds since 1970.";
     return 0;
